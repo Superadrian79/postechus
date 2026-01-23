1c5a06c2c732a3eaacb39ff543baef36b6781793
Superadrian79 <104789711+Superadrian79@users.noreply.github.com>
2026-01-23 10:24:02 -0600

    Add files via upload

---
+++ language/EN.php
@@ -0,0 +1,63 @@
+
+<?php 
+
+	class language 
+	{
+		var $home = 'Home';
+		var $home_about = 'About Us';
+		var $home_sucess = 'Success Stories';
+		var $home_contact= 'Contact';
+
+		var $security = 'Security It';
+		var $security_noc = 'NOC';
+		var $security_soc = 'CSOC';
+		var $security_csirt = 'CSIRT';
+		var $security_assurance = 'Assurance & Hardening';
+		var $security_pentest = 'Pentest';
+		var $security_vulnerability = 'Vulnerability Assesment';
+		var $security_sga = 'Security Gateway SGA';
+
+		var $solutions = 'Solutions';
+		var $solutions_cfi = 'CFI';
+		var $solutions_data = 'Data Mining';
+		var $solutions_monitoring = 'Monitoring';
+		var $solutions_nsm = 'Network Cyber-Security Monitoring - NSM';
+		var $social_media_monitoring  = 'Social Media Monitoring';
+		var $solutions_perimetral = 'Perimetral Security';
+		var $solutions_ids = 'IDS / IPS';
+		var $solutions_analysis = 'Traffic Analysis';
+		var $solutions_reconstruction = 'Traffic Reconstruction';
+		var $solutions_desencription = 'Traffic Desencription';
+		var $solutions_big = 'Big Data Analytics';
+		var $solutions_incident = 'Incident Management';
+		var $solutions_hosting = 'Hosting & Dedicated Servers';
+		var $solutions_domains = 'Domain Check';
+		var $solutions_methodology = 'Methodology';
+		var $solutions_speech = 'Speech Recognition';
+
+		var $training = 'Training';
+		var $training_incident = 'Incident Handling';
+		var $training_techniques = 'Techniques and Tools for Vulnerability Assessment';
+		var $training_pci = 'PCI DSS Fundaments';
+		var $training_network = 'Network Security';
+		var $training_it = 'IT Management Fundamentals';
+		var $training_itil = 'ITIL V 3 Management Services';
+
+		var $partnership = 'Partnerships';
+
+		var $access = 'Access';
+		var $access_email = 'Email';
+		var $access_support = 'Online Support';
+		var $access_pm = 'Group Office';
+
+		var $copyright = 'Postech IT Solution Provider S.A de C.V. © 2019 All Rights Reserved';
+		var $address = 'Montaña 80, Col. Los Pastores, Estado de México, C.P. 53340';
+		var $mail = 'ventas@postech.com.mx';
+		var $phone = '(+52) 4125 0121';
+		var $terms = 'Terms of Use';
+		var $privacy = 'Privacy Policies';
+
+
+	}
+
+ ?>
---
+++ language/ES.php
@@ -0,0 +1,63 @@
+
+<?php 
+
+	class language 
+	{
+		var $home = 'Inicio';
+		var $home_about = 'Nosotros';
+		var $home_sucess = 'Historias de Exito';
+		var $home_contact= 'Contacto';
+
+		var $security = 'Seguridad It';
+		var $security_noc = 'NOC';
+		var $security_soc = 'CSOC';
+		var $security_csirt = 'CSIRT';
+		var $security_assurance = 'Assurance & Hardening';
+		var $security_pentest = 'Pentest';
+		var $security_vulnerability = 'Vulnerability Assesment';
+		var $security_sga = 'Security Gateway SGA';
+
+		var $solutions = 'Soluciones';
+		var $solutions_cfi = 'CFI';
+		var $solutions_data = 'Data Mining';
+		var $solutions_monitoring = 'Monitoreo';
+		var $solutions_nsm = 'Monitoreo de Sistemas y Redes - NSM';
+		var $social_media_monitoring  = 'Monitoreo de Redes Sociales';
+		var $solutions_perimetral = 'Seguridad Perimetral';
+		var $solutions_ids = 'IDS / IPS';
+		var $solutions_analysis = 'Análisis de Tráfico';
+		var $solutions_reconstruction = 'Reconstrucción de Tráfico';
+		var $solutions_desencription = 'Descifrado de Tráfico';
+		var $solutions_big = 'Análisis de Big Data';
+		var $solutions_incident = 'Gestión de Incidentes';
+		var $solutions_hosting = 'Hosting & Servidores Dedicados';
+		var $solutions_domains = 'Disponibilidad de Dominios';
+		var $solutions_methodology = 'Metodología';
+		var $solutions_speech = 'Reconocimiento de Voz';
+
+		var $training = 'Capacitación';
+		var $training_incident = 'Incidentes de Seguridad';
+		var $training_techniques = 'Técnicas y herramientas para evaluación de vulnerabilidad';
+		var $training_pci = 'Fundamentos PCI DSS';
+		var $training_network = 'Seguridad de Red';
+		var $training_it = 'Fundamentos de IT Management';
+		var $training_itil = 'ITIL V 3 Manejo de Servicios';
+
+		var $partnership = 'Alianzas';
+
+		var $access = 'Accesos';
+		var $access_email = 'Email';
+		var $access_support = 'Soporte en Línea';
+		var $access_pm = 'Oficina Virtual';
+
+		var $copyright = 'Postech IT Solution Provider S.A de C.V. © 2019 Todos los Derechos Reservados';
+		var $address = 'Montaña 80, Col. Los Pastores, Estado de México, C.P. 53340';
+		var $mail = 'ventas@postech.com.mx';
+		var $phone = '(+52) 4125 0121';
+		var $terms = 'Términos de Uso';
+		var $privacy = 'Políticas de Privacidad';
+
+
+	}
+
+ ?>
