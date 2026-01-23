5e2f457c7ab2f9fdeba5d9a0da79ee77b6cbbf15
Superadrian <super_adrian79@hotmail.com>
2026-01-23 11:24:08 -0600

    archivos faltantes

---
+++ envio.php
@@ -0,0 +1,169 @@
+<?php
+
+//************************* ENVIO DE EMAIL **********************************************************
+include("mandaemail.inc");
+
+if ( isset($_POST['direccionenvio']) && isset($_POST['paginaconfirmacion']) ) {
+    $mailTo = $_POST['direccionenvio'];
+    $mailFrom = $_POST['email'];
+    $mailSubject = "Formulario de contacto postech.com.mx";
+    $now = date('d-m-Y');
+    $empresa = $_POST['empresa'];
+	$telefono = $_POST['telefono'];
+	$comentarios = $_POST['comentarios'];
+	$name = $_POST['nombre'];
+	$paginac = $_POST['paginaconfirmacion'];
+	
+
+	/** MODIFIQUE AQUI EL CODIGO, INCLUYA UNA LINEA COMO LA DE  Nombre: $nombre Y
+	'** DESPUES \n 
+	'** PARA CADA CAMPO QUE HAYA AGREGADO SIN BORRAR <<<EOT y EOT; E INDIQUE EL
+    FORMATO DEL CORREO */
+$bodycorreo=
+"<table width=\'700\' border=\"0\" bordercolor=\"#333333\" cellpadding=\"0\" cellspacing=\"0\" bgcolor=\"#000000\"><tr><td><table width=\"700\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td><img src=\"http://www.superadrian.com.mx/images/mensaje1.jpg\" width=\"700\" height=\"200\" alt=\"Mensaje Nuevo\" /></td></tr>
+        <tr>
+          <td  bgcolor=\"#000000\">&nbsp;</td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#1f2532\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#ffd200\"><h3>FECHA</h3></font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#000000\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#f4f4f4\">".$now."</font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#1f2532\" align=\"center\">&nbsp;</td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#000000\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#ffd200\"><h3>NOMBRE</h3></font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#1f2532\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#f4f4f4\">".$_POST['name']."</font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#000000\" align=\"center\">&nbsp;</td>
+        </tr>		
+        <tr>
+          <td bgcolor=\"#000000\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#ffd200\"><h3>EMPRESA</h3></font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#1f2532\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#f4f4f4\">".$_POST['empresa']."</font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#000000\" align=\"center\">&nbsp;</td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#1f2532\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#ffd200\"><h3>TELÉFONO</h3></font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#000000\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#f4f4f4\">".$_POST['telefono']."</font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#1f2532\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#ffd200\"><h3>EMAIL</h3></font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#000000\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#f4f4f4\">".$_POST['mailFrom']."</font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#1f2532\">&nbsp;</td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#000000\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#ffd200\"><h3>COMENTARIOS</h3></font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#1f2532\" align=\"center\"><font face=\"Verdana, Geneva, sans-serif\" color=\"#f4f4f4\">".$_POST['comentarios']."</font></td>
+        </tr>
+        <tr>
+          <td bgcolor=\"#1f2532\">&nbsp;</td>
+        </tr>
+        <tr>
+          <td><img src=\"http://www.superadrian.com.mx/images/mensaje2.jpg\" width=\"700\" height=\"72\" alt=\"http://www.superadrian.com.mx/\" /></td>
+        </tr>
+      </table></td>
+    </tr>
+  </table>";
+  
+$mailFormat = 0; // 0 para correo HTML o 1 para correo de texto
+$mailBody = <<<EOT
+/*
+Fecha: $now\n
+Nombre: $name\n
+Empresa: $empresa\n
+Telefono: $telefono\n
+Email: $mailFrom\n
+Cometarios:$comentarios\n
+*/
+$bodycorreo;
+Fin del email
+EOT;
+  
+  
+
+    MandaEmail($mailTo,$mailFrom,$mailSubject,$mailBody,$mailFormat);	  		  		
+    
+    //***MANDA A LA PÁGINA DE CONFIRMACION Y LE PASA LO QUE TRAEN LOS CAMPOS nombre y email
+    
+    Header("Location:". $_POST['paginaconfirmacion'] . "?nombre=" . $_POST['nombre'] . "&email=" . $_POST['email']);
+    
+    //*** FIN MANDA A LA PÁGINA DE CONFIRMACION Y LE PASA LO QUE TRAEN LOS CAMPOS nombre y email
+} else {
+
+$head = '<?xml version="1.0" encoding="UTF-8"?>';
+$head .= <<<EOT
+<!DOCTYPE html 
+     PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>CONTACTENOS</title>
+<style type="text/css">
+<!--
+body { 
+  font-family:"Lucida Grande","Lucida Console",sans-serif;
+}
+#content {
+  font-size:0.8em;
+    position:absolute;
+    width:750px;
+    left:50%;
+    margin-left:-392px;
+    margin-top:3em;
+    padding:15px;
+    border:2px solid gray;
+}
+#content h4 {
+  font-family:"georgia",serif;
+    font-size:1.5em;
+    color:brown;
+    font-variant:small-caps;
+    margin-top:0;
+}
+-->
+</style>
+</head>
+
+<body>
+
+<div id="content">
+<h4>Se han generado los siguientes errores</h4>
+EOT;
+
+	echo $head;
+
+	if(!isset($_POST['direccionenvio'])) {
+    	echo '<p>No se puede enviar el formulario porque el campo "direccionenvio" est&aacute; vac&iacute;o</p> $mailTo';
+    }
+    
+    if(!isset($_POST['paginaconfirmacion'])) {
+    	echo '<p>No se puede enviar el formulario porque el campo "paginaconfirmacion" est&aacute; vac&iacute;o</p>'.$_POST['paginaconfirmacion'];
+    }
+    
+    $bott =<<<EOT
+</div>
+</body>
+</html>
+EOT;
+
+	echo $bott; 
+}
+//**********************FIN ENVIO DE EMAIL **********************************************************
+?>
---
+++ index.php
@@ -0,0 +1,110 @@
+<?php header("Location: EN/index.php"); ?>
+
+<!DOCTYPE html>
+<html dir="ltr" lang="es-MX" xml:lang="es-MX">
+<head>
+
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />	
+  	<meta name="keywords" content="consultoria, servicios, TI, monitoreo, big data, servicios, analisis, seguridad, informatica, seguridad informatica, perimetral, firewall, ids, ips, telecomunicaciones, TICS, BSM, BCM, DRP, BCP, ITIL, Cobit, ISO 27001, ISO 27002, ISO 27035, cert, csirt, noc , soc, seguridad informatica, capacitacion, cursos, TT, TICs, analisis de datos, mineria de datos, dataware, hosting, housing, desarrollo, politicas, normatividad, enisa, first, alapsi, 20/20, trafico, red, reconstruccion, desencripcion, ciber defensa, ciber seguridad, firewall" />
+  	<meta name="rights" content="POSTECH IT SOLUTION PROVIDER" />
+  	<meta name="description" content="Postech IT Solution Provider, Soluciones en monitoreo y seguridad de la información: Hacemos eficiente y productivo su negocio, sin grandes inversiones utilizando código abierto en nuetsras aplicaciones." />
+  	<META NAME="ROBOTS" CONTENT="All">
+
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="favicon-152.png">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="favicon-144.png">
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="favicon-120.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="favicon-114.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" 	 href="favicon-72.png">
+	<link rel="apple-touch-icon-precomposed" sizes="57x57"	 href="favicon-57.png">
+
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<link rel="stylesheet" href="css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="css/responsive.css" type="text/css" />
+
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+<link href="css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+
+
+	<!-- Document Title
+	============================================= -->
+	<title>Postech IT Solution Provider</title>
+
+<style type="text/css">
+
+	body, html {height: 100%; overflow:hidden;}
+	#background { background-image: url('images/bg1.jpg');}
+	#foreground {background-image: url('images/bg2.png');}
+	#fore-foreground {background-image: url('images/bg3.png');}
+	#fore-fore-foreground {background-image: url('images/bg4.png');}
+	.mouse-bg { left: 0; right: 0; bottom: 0; top: 0; position: fixed; z-index: -1; }
+    .design { padding-top: 20px; padding-bottom: 20px; background-color: rgba(0,0,0,.8); text-align: center;}
+	.centrado-porcentual {position: absolute; width: 100%;  left: 50%;  top: 50%; transform: translate(-50%, -50%); -webkit-transform: translate(-50%, -50%); }
+
+</style>
+
+</head>
+
+<body>
+
+
+
+<div class="centrado-porcentual design" style="border-bottom: 1px solid #FFF; border-top: 1px solid #FFF;">
+
+		<a href="ES/index.php"><img src="images/postech-log1.png" class="responsive"></a>
+
+<?php
+$pais="No definido"; $accept = strtolower($_SERVER["HTTP_ACCEPT_LANGUAGE"]); $lang = explode( ",", $accept);
+$idioma_primario = explode('-',$lang[0]); $idioma = $idioma_primario[0]; if($idioma_primario[1]){
+$pais = explode( "-", $lang[0]); $pais = $pais[1]; }
+/*echo "<br />"; 
+echo "El idioma primario es = " . $idioma; */
+echo "<br />"; 
+echo "El código de país es = ". $pais;
+
+if ($idioma=='es'){ $linkr="ES/index.php"; } else { $linkr="EN/index.php"; } $delay = "100"; 
+
+echo "<br><center><b> Idioma detectado es =</b> $idioma</center>"; 
+echo '<meta http-equiv="refresh" content="'.$delay.';url='.$linkr.'">';
+ ?>
+
+</div>
+
+
+<!-- Parallax -->
+
+<div id="background" class="mouse-bg"></div>
+<div id="foreground" class="mouse-bg"></div>
+<div id="fore-foreground"  class="mouse-bg"></div>
+<div id="fore-fore-foreground"  class="mouse-bg"></div>
+<script src="js/jquery.min.js"></script>
+
+<script src="js/mouse.parallax.js"></script>
+
+<script>
+$(document).ready(function() {
+				$('#background').mouseParallax({ moveFactor: 5 });
+				$('#foreground').mouseParallax({ moveFactor: 10 });
+				$('#fore-foreground').mouseParallax({ moveFactor: 15 });
+				$('#fore-fore-foreground').mouseParallax({ moveFactor: 20 });
+				
+				$('body').height(3000);
+			});
+</script>
+
+</body>
+</html>
