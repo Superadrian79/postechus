96a676b2b223927e3f632fb3132800114e21a768
Vianney <mvianney@tssc.mx>
2026-01-23 19:58:18 -0600

    SIEM COMPLIANCE

---
+++ EN/siem_compliance.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="es-MX" xml:lang="es-MX">
+<head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PBGBGN5');</script>
+<!-- End Google Tag Manager -->
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="robots" content="all" /><meta name="googlebot" content="index, follow" />
+  	<meta name="description" content="El ETL ADBConnect es una herramienta para mover datos desde múltiples fuentes, reformatear, limpiar y normalizar los datos para que se puede analizar o adecuar para hacer frente a las necesidades del negocio y promover la interoperabilidad.">
+  	<meta name="keywords" content="data, minning, adb, connect, cualquier base, datos, extraer, transformar, cargar, etl, multiples, fuentes, formatos, reglas">
+  	<meta name="author" content="Postech IT Solutions Provider">	
+	<meta name="viewport" content="width=device-width, initial-scale=1" />	 	
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
+
+
+	
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="../css/style.css" type="text/css" />
+	<link rel="stylesheet" href="../css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="../css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="../css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="../css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="../css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="../css/responsive.css" type="text/css" />
+    <link rel="stylesheet" href="../css/custom26.css" type="text/css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+
+	<!-- Document Title
+	============================================= -->
+	<title> SIEM COMPLIANCE</title>
+
+</head>
+
+<body class="stretched no-transition">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBGBGN5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+<?php
+$path_array = pathinfo($_SERVER['PHP_SELF']);
+$path = explode("/",$path_array['dirname']);
+$path =$path[count($path) - 1];
+include ('../class/navbar.php');
+include ("../language/$path.php");
+$lang=new language();
+$nav=new navbar($path, $lang, $path_array['filename']);
+?>
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title" class="page-title-dark">
+
+			<div class="container clearfix">
+				<h1> SIEM COMPLIANCE</h1>
+				<span>PostecH Cyber Security Solutions</span>
+				<ol class="breadcrumb">
+                	<li><a href="soluciones.php">Tecnologías</a></li>
+					<li class="active"> SIEM COMPLIANCE</li>
+				</ol>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content -->
+		<section id="content" style="margin-bottom: 0px;">
+			<div class="content-wrap">
+				<div class="section header-stick">
+					<div class="container clearfix">
+
+						<div style="text-align:center"><p></p><h3 class="titulo sombra" style="padding-top: 80px;"> SIEM COMPLIANCE</h3><p></p></div>
+
+						<p></p><hr class="hr1"><p></p>
+
+						<div style="background-color: #c1c1c1; padding: 15px; text-align: justify;">
+							<p style="margin: 0;">The Postech Compliance SIEM provides the ability to map the security controls required by standards such as PCI DSS, HIPAA, GDPR, CNBV and others. The solution aggregates and analyzes data from multiple systems, mapping security alerts as compliance requirements.</p>
+						</div>
+
+						<br>
+
+						<div style="text-align: justify;">
+							<p>Compliance SIEM provides a security solution capable of monitoring your infrastructure, detecting threats, intrusion attempts, system anomalies, misconfigured applications, and unauthorized user actions. It also provides a framework for incident response and regulatory compliance.</p>
+							<br>
+							<h3 class="blue">AGENTS</h3>
+							<br>
+							<p>The lightweight agent is designed to perform a number of tasks to detect threats and, when necessary, trigger automatic responses. The main capabilities of the agent are:</p>
+							<br>
+							<p></p><div style="margin-left:15px;">
+								<ul>
+								<li>Collection of data from logs and events</li>
+								<li>Monitoring the integrity of registry keys and files</li>
+								<li>Inventory of running processes and installed applications</li>
+								<li>Monitoring of open ports and network configuration</li>
+								<li>Detection of rootkits or malware artifacts</li>
+								<li>Evaluation of the configuration and monitoring of policies</li>
+								<li>Execution of active responses</li>
+								</ul>
+							</div><p></p>
+							<br>
+							<p>The agents run on many different platforms, including Windows, Linux, Mac OS X, AIX, Solaris, and HP-UX. They can be configured and managed from the server.</p>
+							<br>
+							<h3 class="blue">SERVER</h3>
+							<br>
+							<p>The Postech SIEM server is in charge of analyzing data received from agents, processing events through decoders and rules, and using threat intelligence to search for known IOCs (Indicators of Compromise). A single Postech SIEM server can analyze data from hundreds or thousands of agents and scale out when configured in cluster mode.</p>
+							<br>
+							<p>The server is also used to manage the agents, configuring and updating them remotely when necessary. Furthermore, the server is capable of sending commands to agents, for example, to trigger a response when a threat is detected.</p>
+							<br>
+							<h3 class="blue">Elastic Stack</h3>
+							<p>Alerts generated by SIEM are sent to the Elastic Stack, where they are indexed and stored. The unique integration between SIEM and Kibana (one of the Elastic Stack components), provides a powerful user interface for data visualization and analysis, which can also be used to manage and monitor agent status and configuration .</p>
+							<br>
+							<p>The SIEM for Compliance web user interface includes out-of-the-box dashboards for regulatory compliance (e.g. PCI DSS, GDPR, CIS, NIST 800-53), vulnerable application detection, file integrity monitoring, configuration, security events, cloud infrastructure monitoring and others.</p>
+							<br>
+							<div style="width:1024px; margin-right:auto; margin-left:auto;"><img class="img-responsive" src="../images/siem compliance/com01.jpg"></div><br>
+							<div style="width:181px; margin-right:auto; margin-left:auto;"><img class="img-responsive" src="../images/siem compliance/com02.jpg"></div>
+						</div>
+
+						<br><br>
+						
+						<div class="clear"></div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- #content end -->
+
+<?php
+include ('../class/footer.php');
+$foot=new footer($path, $lang);
+?>
+
+
+
+	</div><!-- #wrapper end -->
+
+
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="../js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="../js/functions.js"></script>
+
+</body>
+</html>
---
+++ ES/siem_compliance.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="es-MX" xml:lang="es-MX">
+<head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PBGBGN5');</script>
+<!-- End Google Tag Manager -->
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="robots" content="all" /><meta name="googlebot" content="index, follow" />
+  	<meta name="description" content="El ETL ADBConnect es una herramienta para mover datos desde múltiples fuentes, reformatear, limpiar y normalizar los datos para que se puede analizar o adecuar para hacer frente a las necesidades del negocio y promover la interoperabilidad.">
+  	<meta name="keywords" content="data, minning, adb, connect, cualquier base, datos, extraer, transformar, cargar, etl, multiples, fuentes, formatos, reglas">
+  	<meta name="author" content="Postech IT Solutions Provider">	
+	<meta name="viewport" content="width=device-width, initial-scale=1" />	 	
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
+
+
+	
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="../css/style.css" type="text/css" />
+	<link rel="stylesheet" href="../css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="../css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="../css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="../css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="../css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="../css/responsive.css" type="text/css" />
+    <link rel="stylesheet" href="../css/custom26.css" type="text/css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+
+	<!-- Document Title
+	============================================= -->
+	<title> SIEM COMPLIANCE</title>
+
+</head>
+
+<body class="stretched no-transition">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBGBGN5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+<?php
+$path_array = pathinfo($_SERVER['PHP_SELF']);
+$path = explode("/",$path_array['dirname']);
+$path =$path[count($path) - 1];
+include ('../class/navbar.php');
+include ("../language/$path.php");
+$lang=new language();
+$nav=new navbar($path, $lang, $path_array['filename']);
+?>
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title" class="page-title-dark">
+
+			<div class="container clearfix">
+				<h1> SIEM COMPLIANCE</h1>
+				<span>PostecH Cyber Security Solutions</span>
+				<ol class="breadcrumb">
+                	<li><a href="soluciones.php">Tecnologías</a></li>
+					<li class="active"> SIEM COMPLIANCE</li>
+				</ol>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content -->
+		<section id="content" style="margin-bottom: 0px;">
+			<div class="content-wrap">
+				<div class="section header-stick">
+					<div class="container clearfix">
+
+
+						<div style="text-align:center"><p></p><h3 class="titulo sombra" style="padding-top: 80px;"> SIEM COMPLIANCE </h3><p></p></div>
+
+						<p></p><hr class="hr1"><p></p>
+
+						<div style="background-color: #c1c1c1; padding: 15px; text-align: justify;">
+							<p style="margin: 0;">El SIEM de cumplimiento Postech proporciona la posibilidad de mapear los controles de seguridad requeridos por estándares como  PCI DSS, HIPAA, GDPR, CNBV  y otros. La solución agrega y analiza datos de múltiples sistemas, mapeando alertas de seguridad como requisitos de los cumplimientos.</p>
+						</div>
+
+						<br>
+
+						<div style="text-align: justify;">
+							<p>El SIEM de cumplimiento proporciona una solución de seguridad capaz de monitorear su infraestructura, detectar amenazas, intentos de intrusión, anomalías del sistema, aplicaciones mal configuradas y acciones de usuarios no autorizados. También proporciona un marco para la respuesta a incidentes y el cumplimiento normativo.</p>
+							<br>
+							<h3 class="blue">AGENTES</h3>
+							<br>
+							<p>El agente ligero está diseñado para realizar una serie de tareas con el objetivo de detectar amenazas y, cuando sea necesario, activar respuestas automáticas. Las capacidades principales del agente son:</p>
+							<br>
+							<p></p><div style="margin-left:15px;">
+								<ul>
+								<li>Recopilación de datos de registros y eventos.</li>
+								<li>Supervisión de la integridad de las claves de registro y archivos.</li>
+								<li>Inventario de procesos en ejecución y aplicaciones instaladas.</li>
+								<li>Monitoreo de puertos abiertos y configuración de red.</li>
+								<li>Detección de rootkits o artefactos de malware.</li>
+								<li>Evaluación de la configuración y seguimiento de políticas.</li>
+								<li>Ejecución de respuestas activas.</li>
+								</ul>
+							</div><p></p>
+							<br>
+							<p>Los agentes se ejecutan en muchas plataformas diferentes, incluidas Windows, Linux, Mac OS X, AIX, Solaris y HP-UX. Se pueden configurar y administrar desde el servidor.</p>
+							<br>
+							<h3 class="blue">SERVIDOR</h3>
+							<br>
+							<p>El servidor de Postech SIEM se encarga de analizar los datos recibidos de los agentes, procesar eventos a través de decodificadores y reglas, y utilizar inteligencia de amenazas para buscar IOC (Indicadores de Compromiso) conocidos. Un solo servidor de Postech SIEM puede analizar datos de cientos o miles de agentes y escalar horizontalmente cuando se configura en modo de clúster.</p>
+							<br>
+							<p>El servidor también se utiliza para gestionar los agentes, configurándolos y actualizándolos de forma remota cuando sea necesario. Además, el servidor es capaz de enviar órdenes a los agentes, por ejemplo, para activar una respuesta cuando se detecta una amenaza.</p>
+							<br>
+							<h3 class="blue">Elastic Stack</h3>
+							<p>Las alertas generadas por el SIEM se envían a Elastic Stack, donde se indexan y almacenan. La integración única entre el SIEM y Kibana (uno de los componentes del Elastic Stack), proporciona una poderosa interfaz de usuario para la visualización y el análisis de datos, que también se puede utilizar para administrar y monitorear la configuración y el estado de los agentes.</p>
+							<br>
+							<p>La interfaz de usuario web del SIEM para cumplimiento incluye paneles de control listos para usar para el cumplimiento normativo (por ejemplo, PCI DSS, GDPR, CIS, NIST 800-53), detección de aplicaciones vulnerables, monitoreo de integridad de archivos, evaluación de configuración, eventos de seguridad, monitoreo de infraestructura en la nube y otros.</p>
+							<br>
+							<div style="width:1024px; margin-right:auto; margin-left:auto;"><img class="img-responsive" src="../images/siem compliance/com01.jpg"></div><br>
+							<div style="width:181px; margin-right:auto; margin-left:auto;"><img class="img-responsive" src="../images/siem compliance/com02.jpg"></div>
+						</div>
+
+						<br><br>
+		
+
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</section>
+		<!-- #content end -->
+
+<?php
+include ('../class/footer.php');
+$foot=new footer($path, $lang);
+?>
+
+
+
+	</div><!-- #wrapper end -->
+
+
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="../js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="../js/functions.js"></script>
+
+</body>
+</html>
