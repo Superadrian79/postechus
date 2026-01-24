fc53513511df36f52740749d2299dac198ea9873
Vianney <mvianney@tssc.mx>
2026-01-23 18:45:44 -0600

    edr

---
+++ EN/edr.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="es-MX" xml:lang="es-MX">
+<head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PBGBGN5');</script>
+<!-- End Google Tag Manager -->
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="robots" content="all" /><meta name="googlebot" content="index, follow" />
+  	<meta name="description" content="El ETL ADBConnect es una herramienta para mover datos desde múltiples fuentes, reformatear, limpiar y normalizar los datos para que se puede analizar o adecuar para hacer frente a las necesidades del negocio y promover la interoperabilidad.">
+  	<meta name="keywords" content="data, minning, adb, connect, cualquier base, datos, extraer, transformar, cargar, etl, multiples, fuentes, formatos, reglas">
+  	<meta name="author" content="Postech IT Solutions Provider">	
+	<meta name="viewport" content="width=device-width, initial-scale=1" />	 	
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
+
+
+	
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="../css/style.css" type="text/css" />
+	<link rel="stylesheet" href="../css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="../css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="../css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="../css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="../css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="../css/responsive.css" type="text/css" />
+    <link rel="stylesheet" href="../css/custom26.css" type="text/css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+
+	<!-- Document Title
+	============================================= -->
+	<title> EDR</title>
+
+</head>
+
+<body class="stretched no-transition">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBGBGN5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+<?php
+$path_array = pathinfo($_SERVER['PHP_SELF']);
+$path = explode("/",$path_array['dirname']);
+$path =$path[count($path) - 1];
+include ('../class/navbar.php');
+include ("../language/$path.php");
+$lang=new language();
+$nav=new navbar($path, $lang, $path_array['filename']);
+?>
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title" class="page-title-dark">
+
+			<div class="container clearfix">
+				<h1> EDR</h1>
+				<span>PostecH Cyber Security Solutions</span>
+				<ol class="breadcrumb">
+                	<li><a href="soluciones.php">Tecnologías</a></li>
+					<li class="active"> EDR</li>
+				</ol>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content -->
+		<section id="content" style="margin-bottom: 0px;">
+			<div class="content-wrap">
+				<div class="section header-stick">
+					<div class="container clearfix">
+
+						<div style="text-align:center"><p></p><h3 class="titulo sombra" style="padding-top: 80px;"> EDR </h3><p></p></div>
+
+						<p></p><hr class="hr1"><p></p>
+
+						<div style="background-color: #c1c1c1; padding: 15px; text-align: justify;">
+							<p>Endpoint Detection and Response. Endpoint detection and response is an emerging technology that addresses the need for continuous monitoring and response to advanced threats. It is a form of advanced threat protection by tracking endpoint and network events and recording the information in a central database where additional analysis can be performed.</p>
+						</div>
+
+						<br><br>
+
+						<div style="text-align: justify;">
+
+							<p>The malware and IT security panorama has undergone a major change in terms of volume and sophistication. There has been an exponential increase in the number of viruses in circulation (around 200,000 new viruses appear every day), and new techniques for penetrating defenses and hiding malware are allowing threats to remain on corporate networks for long periods.</p>
+							<p>At the same time, IT environments have become increasingly complex, making management more difficult and systems more vulnerable. Yet traditional antivirus solutions are out of step with the reality. Their linear evolution continues to use outdated detection techniques based on signature files and heuristic algorithms. This means that the results are inaccurate, i.e., that malware can go undetected and false positives are generated.</p>
+							<p>This discrepancy has led to what has been dubbed the ‘window of opportunity for malware’: the time lapse  between the appearance of a new virus and the release of the antidote by security companies. An increasing gap that is exploited by hackers to get viruses, ransomware, Trojans and other types of malware into corporate networks.</p>
+							<p>Such increasingly common threats can encrypt confidential documents and demand a ransom, or simply collect sensitive data for industrial espionage.</p>
+							<p>Governments, banks and other large companies are bearing the brunt of attacks that traditional antivirus solutions are simply not detecting in time. Our Research Department has analyzed millions of virus samples and the best antivirus products on the market to reveal that 18 percent of malware is undetected in the first 24 hours after it is released, and even after three months, these traditional solutions are still unable to detect 2 percent of malware.</p>
+							<p>The solution to this situation is EDR Security service which can accurately classify every application running in your organization, only allowing legitimate programs to run.</p>
+							<p>To achieve this, we have been working a time on a new security model based on three principles: continuous monitoring of applications on a company’s computers and servers, automatic classification using machine learning on our Big Data platform in the cloud, and finally, our technical experts analyze those applications that haven’t been classified automatically to be certain of the behavior of everything that is run on the company’s systems.</p>
+							
+							<br>
+							<div style="margin-left:auto; margin-right:auto; width:635px;"><img class="img-responsive" src="../images/edr/EDR1.jpg"></div>
+
+						</div>
+
+
+						<div style="text-align: justify;">
+							<!-- <p><img class="img-responsive" src="../images/levels03.png"></p> -->
+						</div>
+						<div class="clear"></div>		
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- #content end -->
+
+<?php
+include ('../class/footer.php');
+$foot=new footer($path, $lang);
+?>
+
+
+
+	</div><!-- #wrapper end -->
+
+
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="../js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="../js/functions.js"></script>
+
+</body>
+</html>
---
+++ ES/edr.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="es-MX" xml:lang="es-MX">
+<head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PBGBGN5');</script>
+<!-- End Google Tag Manager -->
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="robots" content="all" /><meta name="googlebot" content="index, follow" />
+  	<meta name="description" content="El ETL ADBConnect es una herramienta para mover datos desde múltiples fuentes, reformatear, limpiar y normalizar los datos para que se puede analizar o adecuar para hacer frente a las necesidades del negocio y promover la interoperabilidad.">
+  	<meta name="keywords" content="data, minning, adb, connect, cualquier base, datos, extraer, transformar, cargar, etl, multiples, fuentes, formatos, reglas">
+  	<meta name="author" content="Postech IT Solutions Provider">	
+	<meta name="viewport" content="width=device-width, initial-scale=1" />	 	
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
+
+
+	
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="../css/style.css" type="text/css" />
+	<link rel="stylesheet" href="../css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="../css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="../css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="../css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="../css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="../css/responsive.css" type="text/css" />
+    <link rel="stylesheet" href="../css/custom26.css" type="text/css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+
+	<!-- Document Title
+	============================================= -->
+	<title> EDR</title>
+
+</head>
+
+<body class="stretched no-transition">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBGBGN5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+<?php
+$path_array = pathinfo($_SERVER['PHP_SELF']);
+$path = explode("/",$path_array['dirname']);
+$path =$path[count($path) - 1];
+include ('../class/navbar.php');
+include ("../language/$path.php");
+$lang=new language();
+$nav=new navbar($path, $lang, $path_array['filename']);
+?>
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title" class="page-title-dark">
+
+			<div class="container clearfix">
+				<h1> EDR</h1>
+				<span>PostecH Cyber Security Solutions</span>
+				<ol class="breadcrumb">
+                	<li><a href="soluciones.php">Tecnologías</a></li>
+					<li class="active"> EDR</li>
+				</ol>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content -->
+		<section id="content" style="margin-bottom: 0px;">
+			<div class="content-wrap">
+				<div class="section header-stick">
+					<div class="container clearfix">
+
+						<div style="text-align:center"><p></p><h3 class="titulo sombra" style="padding-top: 80px;"> EDR </h3><p></p></div>
+
+						<p></p><hr class="hr1"><p></p>
+
+						<div style="background-color: #c1c1c1; padding: 15px; text-align: justify;">
+							<p>Endpoint Detection and Response.  La detección y respuesta en endpoints es una tecnología emergente que aborda la necesidad de monitoreo y respuesta continuos a amenazas avanzadas.  Es una forma de protección avanzada contra amenazas mediante el seguimiento de los eventos de la red  y del punto final y el registro de la información en una base de datos central donde se pueden realizar análisis adicionales.</p>
+						</div>
+
+						<br><br>
+
+						<div style="text-align: justify;">
+
+							<p>El panorama del malware y la seguridad informática ha experimentado un cambio importante en términos de volumen y sofisticación. por ello existe un aumento exponencial en el número de virus que circulan (alrededor de 200.000 nuevos virus aparecen cada día), y nuevas técnicas para penetrar defensas y ocultar malware permiten que las amenazas permanezcan en las redes de la empresa durante largos períodos.</p>
+							<p>Al mismo tiempo, los entornos de TI se han vuelto cada vez más complejos, lo que dificulta la gestión y sistemas más vulnerables. Sin embargo, las soluciones antivirus tradicionales están fuera de sintonía con la realidad. Su evolución lineal sigue utilizando obsoletas técnicas de detección basadas en archivos de firmas y algoritmos heurísticos. Esto significa que los resultados son inexactos, es decir; que el malware puede pasar desapercibido y ser un falso positivo.</p>
+							<p>Esta discrepancia ha llevado a lo que se denomina "Ventana de oportunidad para el malware": el lapso de tiempo entre la aparición de un nuevo virus y la liberación del antídoto por las empresas de seguridad. Una brecha creciente que es explotado por piratas informáticos para obtener virus, ransomware, Troyanos y otros tipos de malware en las redes de empresas.</p>
+							<p>Estas amenazas cada vez más comunes pueden cifrar documentos confidenciales y exigir un rescate, o simplemente recopilar datos sensibles para el espionaje industrial.</p>
+							<p>Los gobiernos, los bancos y otras grandes empresas soportan la peor parte de los ataques que los antivirus tradicionales y las soluciones simplemente no detectan a tiempo. Nuestra investigación ha analizado millones de muestras de virus y los mejores productos antivirus del mercado para revelar que el 18 por ciento del malware no se detecta en las primeras 24 horas después de su lanzamiento, e incluso después de tres meses. Las soluciones tradicionales aún no pueden detectar el 2 por ciento de malware.</p>
+							<p>La solución a esta situación es EDR, un servicio de seguridad que puede clasificar con precisión cada aplicación que se ejecuta en su organización, y solo permite programas legítimos para ejecutar.</p>
+							<p>Para lograrlo, llevamos tiempo trabajando en un nuevo modelo de seguridad basado en tres principios: continuo monitoreo de aplicaciones en las computadoras de una empresa y servidores, clasificación automática mediante aprendizaje automático en nuestra plataforma de Big Data en la nube, y finalmente, nuestra técnica donde los expertos analizan aquellas aplicaciones que no han sido clasificadas automáticamente para estar seguros del comportamiento de todo lo que se ejecuta en los sistemas de las empresas.</p>
+
+							<br>
+							<div style="margin-left:auto; margin-right:auto; width:635px;"><img class="img-responsive" src="../images/edr/EDR1.jpg"></div>
+
+						</div>
+
+						<div style="text-align: justify;">
+							<!-- <p><img class="img-responsive" src="../images/levels03.png"></p> -->
+						</div>
+
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</section>
+		<!-- #content end -->
+
+<?php
+include ('../class/footer.php');
+$foot=new footer($path, $lang);
+?>
+
+
+
+	</div><!-- #wrapper end -->
+
+
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="../js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="../js/functions.js"></script>
+
+</body>
+</html>
