683377af5f36c29726a485109ceee5117c04d699
Vianney <mvianney@tssc.mx>
2026-01-23 19:13:10 -0600

    siem y mdm

---
+++ ES/mdm.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="es-MX" xml:lang="es-MX">
+<head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PBGBGN5');</script>
+<!-- End Google Tag Manager -->
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="robots" content="all" /><meta name="googlebot" content="index, follow" />
+  	<meta name="description" content="El ETL ADBConnect es una herramienta para mover datos desde múltiples fuentes, reformatear, limpiar y normalizar los datos para que se puede analizar o adecuar para hacer frente a las necesidades del negocio y promover la interoperabilidad.">
+  	<meta name="keywords" content="data, minning, adb, connect, cualquier base, datos, extraer, transformar, cargar, etl, multiples, fuentes, formatos, reglas">
+  	<meta name="author" content="Postech IT Solutions Provider">	
+	<meta name="viewport" content="width=device-width, initial-scale=1" />	 	
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
+
+
+	
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="../css/style.css" type="text/css" />
+	<link rel="stylesheet" href="../css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="../css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="../css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="../css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="../css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="../css/responsive.css" type="text/css" />
+    <link rel="stylesheet" href="../css/custom26.css" type="text/css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+
+	<!-- Document Title
+	============================================= -->
+	<title> MDM</title>
+
+</head>
+
+<body class="stretched no-transition">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBGBGN5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+<?php
+$path_array = pathinfo($_SERVER['PHP_SELF']);
+$path = explode("/",$path_array['dirname']);
+$path =$path[count($path) - 1];
+include ('../class/navbar.php');
+include ("../language/$path.php");
+$lang=new language();
+$nav=new navbar($path, $lang, $path_array['filename']);
+?>
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title" class="page-title-dark">
+
+			<div class="container clearfix">
+				<h1> MDM</h1>
+				<span>PostecH Cyber Security Solutions</span>
+				<ol class="breadcrumb">
+                	<li><a href="soluciones.php">Tecnologías</a></li>
+					<li class="active"> MDM</li>
+				</ol>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content -->
+		<section id="content" style="margin-bottom: 0px;">
+			<div class="content-wrap">
+				<div class="section header-stick">
+					<div class="container clearfix">
+
+						<div style="text-align:center"><p></p><h3 class="titulo sombra" style="padding-top: 80px;"> MDM </h3><p></p></div>
+
+						<p></p><hr class="hr1"><p></p>
+
+						<div style="background-color: #c1c1c1; padding: 15px; text-align: justify;">
+							<p style="margin: 0;">La administración de dispositivos móviles (MDM) es un tipo de software de seguridad utilizado por un departamento de TI para monitorear, administrar y proteger los dispositivos móviles de los empleados (computadoras portátiles, teléfonos inteligentes, tabletas, etc.) que se implementan en múltiples proveedores de servicios móviles y en múltiples dispositivos móviles. sistemas operativos que se utilizan en las organizaciónes.</p>
+						</div>
+
+						<br>
+
+						<div style="text-align: justify;">
+							<p>La gestión de dispositivos móviles (MDM) es el proceso de gestión de dispositivos portátiles móviles, principalmente en términos de uso y seguridad. Los dispositivos móviles se administran a través de una estrategia que rastrea la información esencial sobre cada dispositivo, determina qué aplicaciones se pueden instalar y protege de forma remota los dispositivos móviles en caso de pérdida o robo. MDM también puede rastrear la ubicación del dispositivo móvil tanto por usuario como por ubicación geográfica.</p>
+							<br>
+							<p>En algunos entornos corporativos, los dispositivos móviles han reemplazado por completo a las computadoras de escritorio tradicionales, un cambio drástico hecho posible por la computación en la nube. Cuando una empresa ejecuta todo desde la nube, los empleados pueden acceder a los datos, las aplicaciones y la red de la empresa prácticamente en cualquier lugar donde haya una conexión a Internet. Esta facilidad de uso es exactamente la razón por la que la administración de dispositivos móviles es crucial.</p>
+							<br>
+							<div style="width:300px; margin-right:auto; margin-left:auto;"><img class="img-responsive" src="../images/mdm/mdm1.jpg"></div>
+						</div>
+
+						<br><br>
+
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</section>
+		<!-- #content end -->
+
+<?php
+include ('../class/footer.php');
+$foot=new footer($path, $lang);
+?>
+
+
+
+	</div><!-- #wrapper end -->
+
+
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="../js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="../js/functions.js"></script>
+
+</body>
+</html>
---
+++ ES/siem.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="es-MX" xml:lang="es-MX">
+<head>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PBGBGN5');</script>
+<!-- End Google Tag Manager -->
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="robots" content="all" /><meta name="googlebot" content="index, follow" />
+  	<meta name="description" content="El ETL ADBConnect es una herramienta para mover datos desde múltiples fuentes, reformatear, limpiar y normalizar los datos para que se puede analizar o adecuar para hacer frente a las necesidades del negocio y promover la interoperabilidad.">
+  	<meta name="keywords" content="data, minning, adb, connect, cualquier base, datos, extraer, transformar, cargar, etl, multiples, fuentes, formatos, reglas">
+  	<meta name="author" content="Postech IT Solutions Provider">	
+	<meta name="viewport" content="width=device-width, initial-scale=1" />	 	
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico" />
+
+
+	
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="../css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="../css/style.css" type="text/css" />
+	<link rel="stylesheet" href="../css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="../css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="../css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="../css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="../css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="../css/responsive.css" type="text/css" />
+    <link rel="stylesheet" href="../css/custom26.css" type="text/css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+
+	<!-- Document Title
+	============================================= -->
+	<title> SIEM</title>
+
+</head>
+
+<body class="stretched no-transition">
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBGBGN5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+<?php
+$path_array = pathinfo($_SERVER['PHP_SELF']);
+$path = explode("/",$path_array['dirname']);
+$path =$path[count($path) - 1];
+include ('../class/navbar.php');
+include ("../language/$path.php");
+$lang=new language();
+$nav=new navbar($path, $lang, $path_array['filename']);
+?>
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title" class="page-title-dark">
+
+			<div class="container clearfix">
+				<h1> SIEM</h1>
+				<span>PostecH Cyber Security Solutions</span>
+				<ol class="breadcrumb">
+                	<li><a href="soluciones.php">Tecnologías</a></li>
+					<li class="active"> SIEM</li>
+				</ol>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content -->
+		<section id="content" style="margin-bottom: 0px;">
+			<div class="content-wrap">
+				<div class="section header-stick">
+					<div class="container clearfix">
+
+						<div style="text-align:center"><p></p><h3 class="titulo sombra" style="padding-top: 80px;"> SIEM </h3><p></p></div>
+
+						<p></p><hr class="hr1"><p></p>
+
+						<div style="background-color: #c1c1c1; padding: 15px; text-align: justify;">
+							<p>Security Information and Event Management (SIEM) ayuda a los equipos de seguridad a detectar con precisión y priorizar las amenazas en toda la empresa, además de proporcionar información inteligente que permite a los equipos responder rápidamente para reducir el impacto de los incidentes. Mediante la consolidación de eventos de registro y datos de flujo de red de miles de dispositivos, terminales y aplicaciones distribuidos por toda la red.</p>
+						</div>
+
+						<br>
+
+						<div style="text-align: justify;">
+							<p>Empresas e Instituciones gubernamentales han adoptado aplicaciones en la nube, multinube e hibridas para sus procesos comerciales y de TI. Esta arquitectura ofrece a las empresas el mejor valor en términos de costo, disponibilidad y flexibilidad; sin embargo, también presenta un riesgo adicional, sin considerar la escasez crítica de profesionales de ciberseguridad experimentados y el elevado costo que esto representa.</p>
+							<br>
+							<p>La plataforma SIEM Postech® aborda ambos desafíos. Al automatizar las operaciones de seguridad con capacidades de respuesta y permitir integrar herramientas de automatización de orquestación de seguridad inteligente (SOAR), mientras que sus capacidades analíticas avanzadas reducen el ruido, ajustan las alertas e identifican amenazas tanto dentro como fuera de la red.</p>
+							<br>
+							<p>El SIEM ayuda a los equipos de seguridad a detectar con precisión y priorizar las amenazas en toda la red en tiempo real, además de proporcionar información inteligente que permite a los equipos responder rápidamente para reducir el impacto de los incidentes. Mediante la consolidación de eventos de registro y datos de flujo de red de miles de dispositivos, terminales y aplicaciones distribuidos por toda la red.</p>
+							<br>
+							<p></p>
+							<br>
+							<div style="width:1024px; margin-right:auto; margin-left:auto;"><img class="img-responsive" src="../images/siem/siem1.jpg"></div>
+						</div>
+
+						<br><br>
+
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</section>
+		<!-- #content end -->
+
+<?php
+include ('../class/footer.php');
+$foot=new footer($path, $lang);
+?>
+
+
+
+	</div><!-- #wrapper end -->
+
+
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="../js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="../js/functions.js"></script>
+
+</body>
+</html>
